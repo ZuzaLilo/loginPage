b663d5d0a51241e09eede1852be2572b7c708b97
Zuzanna Debska <zuzanna.debska4321@gmail.com>
2019-09-23 14:23:48 +0200

    updated PDO connection file

--- src/connect.php
+++ src/connect.php
@@ -5,4 +5,18 @@ $db_user = "myuser";
 $db_password = "secret";
 $db_name = "mydb";
 
+try
+    {
+        $connection = new PDO('mysql:host=database;dbname=mydb;charset=utf8mb4', 'myuser', 'secret', 
+        [
+            PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
+        
+    }
+    catch(PDOException $e) {
+        echo "Error: " . $e->getMessage();
+        exit('Database error');
+    }
+
 ?>
--- src/list.php
+++ src/list.php
@@ -4,10 +4,18 @@ session_start();
 
 if(isset($_POST['username']))
 {
-    $login = filter_input(INPUT_POST, 'username');
+    $username = filter_input(INPUT_POST, 'username');
     $password = filter_input(INPUT_POST, 'password');
 
-    echo $login." ".$password;
+    //echo $login." ".$password;
+
+    require_once('./connect.php');
+
+    $userInput = $connection->prepare("SELECT * FROM user_table where username = :username");
+    $userInput->bindValue(':username', $username, PDO::PARAM_STR);
+    $userInput->execute();
+
+    echo $userInput->rowCount();
 }
 else{
     header('Location: admin.php');
